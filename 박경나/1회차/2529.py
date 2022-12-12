f3a4180f111782a74152c550b53ed57c5c24c854
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2022-12-12 23:39:47 +0900

    add: Add comment to 2529

--- 박경나/1회차/2529.py
+++ 박경나/1회차/2529.py
@@ -9,9 +9,9 @@ def dfs(idx, string):
 
     if(idx == k+1):
         if(len(min) == 0):
-            min = string
+            min = string # 처음 호출한 값
         else:
-            max = string
+            max = string # 마지막 호출한 값
         return
 
     for i in range(len(num)):
@@ -22,17 +22,25 @@ def dfs(idx, string):
                 visited[i] = 0
 
 # 새로 숫자가 추가될 때 앞에 숫자를 비교
+# 여기서 중복 방지 체크!
 def ch(i, j, bdh):
+    if bdh == "<":
+        return i < j
     if bdh == ">":
-        return i > j # False
-    else:
-        print(i<j)
-        return i < j # True
+        return i > j
+    return True
 
 dfs(0, "")
 print("%s\n%s" %(max, min))
 
 '''
 메모리/시간(Python3)
-30616KB	132ms	
+30616KB	132ms
+
+부등호를 더이상 만족하지 않으면 재귀함수를 할 필요가 없다 -> 가지치기
+
+숫자, 부등호 담는 리스트 분리
+부등호 인덱스 주의! 체크할 때 n과 n+1을 검사한다고 치면 인덱스 n-1해야함!
+
+ch 함수 : 부등호 파악하는 함수이며 만약 j자리에 비교할 문자가 없는 경우 종료
 '''
