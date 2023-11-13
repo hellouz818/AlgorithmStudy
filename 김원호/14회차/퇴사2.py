e1b33a10417a8866ea07a39b89ea971510dcac7c
wonho.kim2 <wonho.kim2@navercorp.com>
2023-11-13 20:09:16 +0900

    fix: Update

--- 김원호/14회차/퇴사2.py
+++ 김원호/14회차/퇴사2.py
@@ -2,6 +2,7 @@ N = int(input())
 dp = [0 for _ in range(N+1)]
 for i in range(N):
     t, p = map(int, input().split())
-    for ti in range(t, N - i):
-        dp[i + ti] = max(dp[i + ti], dp[i] + p)
-print(dp)
+    dp[i] = max(dp[i], dp[i-1])
+    if i + t <= N:
+        dp[i + t] = max(dp[i + t], dp[i] + p)
+print(max(dp[-2:]))
